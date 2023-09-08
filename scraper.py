ed65beb13ae07a8d96437942b5fa35717b45a9a6
Oscar Centeno <oscarcenteno@users.noreply.github.com>
2023-09-08 18:45:26 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,25 +1,7 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+# open new browser
+browser = uc.Chrome()
 
 driver.get('http://nytimes.com')
 print(driver.title)
